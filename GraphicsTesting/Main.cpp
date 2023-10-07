5738758285a270828e13930f33b6d7e62c187ad9
Joseph Goold <gooldjoseph@gmail.com>
2023-10-07 19:00:32 +0100

    Added input system abstraction May make it automatically initialize when window is created, unsure if this is a good idea or not.

--- GraphicsTesting/Main.cpp
+++ GraphicsTesting/Main.cpp
@@ -1,13 +1,22 @@
 #include "Renderer/Renderer.h"
 
+#include <iostream>
+
 int main()
 {
 	Window* gameWindow = new Window("Graphics Testing", 500, 500);
 	GLFWwindow* window = gameWindow->GetWindow();
 
+	Input::Init(gameWindow);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		gameWindow->ClearWindow();
+
+		if(Input::IsKeyPressed(GLFW_KEY_W, false))
+		{
+			std::cout << "W is pressed" << std::endl;
+		}
 
 		gameWindow->UpdateWindow();
 	}
